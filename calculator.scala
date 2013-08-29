9bc58f9557a184913097db36badab236dc93bd3b
Chuck Callebs <cacallebs@gmail.com>
2013-08-29 00:55:34 -0500

    add sum method to calculator.

--- calculator.scala
+++ calculator.scala
@@ -14,7 +14,13 @@ def divide(x: Int, y: Int) = {
   println(x / y)
 }
 
+def sum(numbers: Array[Int])= {
+  val sum = numbers.foldLeft(0)(_ + _)
+  println(sum)
+}
+
 add(3, 5)
 subtract(5, 3)
 multiply(2, 3)
 divide(20, 5)
+sum(Array(2, 25, 3))
