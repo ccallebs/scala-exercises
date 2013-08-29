24a9e3e0f70922a3f22e4be0f865edecb7321e64
Chuck Callebs <cacallebs@gmail.com>
2013-08-29 00:41:46 -0500

    add calculator.scala to memorize method syntax.

---
+++ calculator.scala
@@ -0,0 +1,20 @@
+def add(x: Int, y: Int) = {
+  println(x + y)
+}
+
+def subtract(x: Int, y: Int) = {
+  println(x - y)
+}
+
+def multiply(x: Int, y: Int) = {
+  println(x * y)
+}
+
+def divide(x: Int, y: Int) = {
+  println(x / y)
+}
+
+add(3, 5)
+subtract(5, 3)
+multiply(2, 3)
+divide(20, 5)
